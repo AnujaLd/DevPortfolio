37b04339093f6bf71294dcd4ec8134d9867d9327
AnujaLd <isanujadewnim@gmail.com>
2024-09-24 15:54:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,51 +10,46 @@ import Project from "./Components/Projects";
 import MoveToTop from "./Components/MoveToTop";
 import Lottie from  "lottie-react";
 import nightsky from "./LottieFiles/night-sky.json";
-import HashLoader from "react-spinners/HashLoader";
+import BeatLoader from "react-spinners/BeatLoader";
 
 function App() {
-  const[Loading,SetLoading]=useState(true);
-
-  useEffect(()=>{
-    SetLoading(true)
-
-    setTimeout(()=>{
-    SetLoading(false)}
-    ,1900)
-  },[])  
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1900);
+  }, []);
   
   return (
     <>
-      {Loading ? (
-      <div className="loader"> 
-        <HashLoader
-          color={'#9067C6'}
-          loading={true}
-          size={100}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-        />
-      </div>
-      ):(
-      <div>
-      
-      <Lottie className="bg" animationData={nightsky} loop={true} />  
-      <Lottie className="bgtwo" animationData={nightsky} loop={true} />   
-      <Lottie className="bgtemp" animationData={nightsky} loop={true} /> 
-
-      <Nav/>
-      <MoveToTop/>
-
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/About" element={<About/>}/>
-        <Route path="/Experience" element={<Experience/>}/>
-        <Route path="/Project" element={<Project/>}/>
-        <Route path="/Resume" element={<Resume/>}/>
-      </Routes>
-      
-      <Footer/>
-      </div>
+      {loading ? (
+        <div className="loader"> 
+          <BeatLoader
+            color={'#9067C6'}
+            loading={true}
+            size={20}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <div>
+          <Lottie className="bg" animationData={nightsky} loop={true} />  
+          <Lottie className="bgtwo" animationData={nightsky} loop={true} />   
+          <Lottie className="bgtemp" animationData={nightsky} loop={true} /> 
+          <Nav/>
+          <MoveToTop/>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/About" element={<About/>}/>
+            <Route path="/Experience" element={<Experience/>}/>
+            <Route path="/Project" element={<Project/>}/>
+            <Route path="/Resume" element={<Resume/>}/>
+          </Routes>
+          <Footer/>
+        </div>
       )}  
     </>
   );
