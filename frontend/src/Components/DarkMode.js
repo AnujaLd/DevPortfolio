690f3517d073ccdda6473d4f8cab2c5a9a796018
AnujaLd <isanujadewnim@gmail.com>
2024-10-09 09:24:19 +0530

    add Dark Mood

--- frontend/src/Components/DarkMode.js
+++ frontend/src/Components/DarkMode.js
@@ -1,22 +1,38 @@
-import React from "react";
-import {ReactComponent as Sun} from '../images/Sun.svg';
-import { ReactComponent as Moon} from '../images/Moon.svg';
+import React, { useState, useEffect } from "react";
+import { ReactComponent as Sun } from '../images/Sun.svg';
+import { ReactComponent as Moon } from '../images/Moon.svg';
 import "../index.css";
 
 const DarkMode = () => {
-    return (
-        <div className='dark_mode'>
-            <input
-                className='dark_mode_input'
-                type='checkbox'
-                id='darkmode-toggle'
-            />
-            <label className='dark_mode_label' for='darkmode-toggle'>
-                <Sun />
-                <Moon />
-            </label>
-        </div>
-    );
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <div className='dark_mode'>
+      <input
+        className='dark_mode_input'
+        type='checkbox'
+        id='darkmode-toggle'
+        onChange={toggleDarkMode}
+        checked={darkMode}
+      />
+      <label className='dark_mode_label' htmlFor='darkmode-toggle'>
+        <Sun />
+        <Moon />
+      </label>
+    </div>
+  );
 };
 
 export default DarkMode;
